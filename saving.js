4adc56510b9a2742f540438ae394fde9f3732ca2
Ruwanya Hettiarachchi <ruwanyahettiarachchi@gmail.com>
2024-04-17 13:00:07 +0530

    first commit

---
+++ saving.js
@@ -0,0 +1,46 @@
+import "./saving.css";
+
+
+function Saving(){
+
+    return(
+        <div>
+
+            <div className = "para1">
+            <p>
+            * Congratulations On Setting Your Savings Target! *<br></br><br></br>
+            Great Job Taking That First Step Towards Financial Success! As A Reward For Your Dedication, You're Now Enrolled To Receive a
+            pesonalized saving tips straight. To your email inbox <br></br><br></br>
+            These tips are designed to help you achieve your savings goal with ease.
+            From Budgeting Hacks To Smart Spending Strategies, We've Got You Covered!<br></br><br></br>
+            Keep An Eye On Your Email For Your First Batch Of Helpful Tips, And Get
+            Ready To Supercharge Your Savings Journey!<br></br><br></br>
+            Here's To Reaching Your Goals And Securing Your Financial Future!
+            </p>
+            </div>
+
+            <br></br>
+
+            <button className="btnBack">BACK TO DASHBOARD</button>
+            
+
+            <br></br>
+
+            <p className="help">
+                Need help? - Talk to us (abc@gmail.com)
+            </p>
+
+
+
+
+        </div>
+
+
+
+
+    )
+
+}
+
+export default Saving;
+
